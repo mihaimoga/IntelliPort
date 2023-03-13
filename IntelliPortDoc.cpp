64791144f01452b95b7b0246c3bbab53853c3344
Stefan-Mihai Moga <stefan-mihai.moga@nxp.com>
2023-03-13 09:25:48 +0200

    Updated copyright header.

--- IntelliPortDoc.cpp
+++ IntelliPortDoc.cpp
@@ -1,4 +1,4 @@
-/* This file is part of IntelliPort application developed by Mihai MOGA.
+/* This file is part of IntelliPort application developed by Stefan-Mihai MOGA.
 
 IntelliPort is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the Open
