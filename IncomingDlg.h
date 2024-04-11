16fd4a67a9b0235fb841d7f1a3c1c20b7ccf3790
Stefan-Mihai MOGA <stefan-mihai@moga.doctor>
2024-04-11 21:36:19 +0300

    Updated copyright information

--- IncomingDlg.h
+++ IncomingDlg.h
@@ -1,3 +1,17 @@
+/* Copyright (C) 2014-2024 Stefan-Mihai MOGA
+This file is part of IntelliPort application developed by Stefan-Mihai MOGA.
+
+IntelliPort is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Open
+Source Initiative, either version 3 of the License, or any later version.
+
+IntelliPort is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+IntelliPort. If not, see <http://www.opensource.org/licenses/gpl-3.0.html>*/
+
 #pragma once
 
 // CIncomingDlg dialog
