7eb6f77fedb1b6f36721f2ee0c0628947295f783
Stefan-Mihai MOGA <stefan-mihai@moga.doctor>
2025-02-27 22:11:12 +0200

    Updated the screenshot

--- hlp/HTMLDefines.h
+++ hlp/HTMLDefines.h
@@ -66,6 +66,10 @@
 
 #pragma once
 
+#include <__atlmfc_core.h>
+#pragma warning(push)
+#pragma warning(disable : _ATLMFC_DISABLED_WARNINGS)
+
 #ifdef _AFX_MINREBUILD
 #pragma component(minrebuild, off)
 #endif
@@ -308,5 +312,8 @@
 #ifdef _AFX_MINREBUILD
 #pragma component(minrebuild, on)
 #endif
+
+// _ATLMFC_DISABLED_WARNINGS
+#pragma warning(pop)
 
 #endif // __AFX_HH_H__
