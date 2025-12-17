aaa664ff69301ff4ee00b61a9da1e9498a4b2039
Stefan-Mihai MOGA <stefan-mihai@moga.doctor>
2025-12-17 04:16:22 +0200

    Updated copyright information.
    Signed-off-by: Stefan-Mihai MOGA <stefan-mihai@moga.doctor>

--- WebBrowserDlg.cpp
+++ WebBrowserDlg.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014-2025 Stefan-Mihai MOGA
+/* Copyright (C) 2014-2026 Stefan-Mihai MOGA
 This file is part of IntelliPort application developed by Stefan-Mihai MOGA.
 IntelliPort is an alternative Windows version to the famous HyperTerminal!
 
